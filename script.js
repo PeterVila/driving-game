3060266e2a07214186834bec2d35f114300e7a7f
Peter Vila <peterdvila@gmail.com>
2021-09-16 21:55:19 +0000

    Finished issue 1 of see-car

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var data = {
-    facing: 'north',
-    // start: false
-}
+  facing: 'north',
+  // start: false
+  angle: 0
+};
 
-var car = document.querySelector('img')
-document.addEventListener('keydown', function(){
-    console.log(event.key)
-    if (event.key === "ArrowRight"){
-        car.className = "right"
-        console.log('car turns 90deg')
-        data.facing = "east"
-    }
-})
+var car = document.querySelector('img');
+
+document.addEventListener('keydown', function () {
+  if (event.key === 'ArrowRight') {
+    car.className = 'right';
+    data.angle += 90;
+  } else if (event.key === 'ArrowLeft') {
+    car.className = 'left';
+  } else if (event.key === 'ArrowDown') {
+    car.className = 'down';
+  } else if (event.key === 'ArrowUp') {
+    car.className = 'up';
+  }
+});
