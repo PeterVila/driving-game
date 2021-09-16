cbc2cdba2bb8b39373154bbf99b3d928eed1990c
Peter Vila <peterdvila@gmail.com>
2021-09-16 22:13:08 +0000

    Added turning functionality for user

---
+++ script.js
@@ -0,0 +1,27 @@
+var data = {
+  facing: 'north',
+  // start: false
+};
+
+var car = document.querySelector('img');
+
+document.addEventListener('keydown', function () {
+  if (event.key === 'ArrowRight') {
+    car.className = 'right';
+    data.facing = "east"
+  } else if (event.key === 'ArrowLeft') {
+    car.className = 'left';
+    data.facing = "west"
+  } else if (event.key === 'ArrowDown') {
+    if (data.facing === "west"){
+        car.className = 'down-alternate'
+    } else {
+        car.className = 'down';
+    }
+    data.facing = "south"
+  } else if (event.key === 'ArrowUp') {
+    car.className = 'up';
+    data.facing = "north"
+  }
+  console.log(data)
+});
